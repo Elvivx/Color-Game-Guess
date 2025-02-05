6078061c7deb1fed106f244fc2829e62d19e1593
Elvis <Elvisraphael00@gmail.com>
2025-02-05 10:48:38 +0100

    Finishing touches

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,14 @@ const ColorGame: React.FC = () => {
   const [options, setOptions] = useState<string[]>([])
   const [power, setPower] = useState<string[]>(["⚡", "⚡", "⚡", "⚡", "⚡"])
   const [score, setScore] = useState<number>(0)
-
-  useEffect(() => {
-    play()
-  }, [])
+  const [status, setStatus] = useState<string>("")
 
   const play = () => {
     if (power.length === 0) return
     const correctColor = generateRandomColor()
     const colorOptions = [correctColor]
 
-    while (colorOptions.length < 4) {
+    while (colorOptions.length < 6) {
       const newColor = generateRandomColor()
       if (!colorOptions.includes(newColor)) {
         colorOptions.push(newColor)
@@ -50,35 +47,51 @@ const ColorGame: React.FC = () => {
     if (selectedColor === targetColor) {
       setScore(score + 1)
       play()
+      stat("Correct🎉")
     } else {
       setPower((prevPower) => prevPower.slice(0, -1))
       play()
+      stat("Wrong❌")
     }
   }
 
   const newGame = () => {
     setScore(0)
+    play()
     setPower(["⚡", "⚡", "⚡", "⚡", "⚡"])
   }
-
+  const stat = (mess: string) => {
+    setStatus(mess)
+    setTimeout(() => {
+      setStatus("")
+    }, 1000)
+  }
   return (
     <main>
       <div className='container'>
         <h1>Color Guessing Game</h1>
-        <div className='colorBox' style={{ backgroundColor: targetColor }}></div>
+        <div className='colorBox' style={{ backgroundColor: targetColor }} data-testid='colorBox'></div>
+        {status && (
+          <p className='status' data-testid='gameStatus'>
+            {status}
+          </p>
+        )}
         <p>Guess the color:</p>
         <div className='options'>
           {options.map((color) => (
-            <button key={color} className='option' onClick={() => handleGuess(color)} style={{ background: color }}>
-              {color}
+            <button key={color} className='option' onClick={() => handleGuess(color)} style={{ background: color }} data-testid='colorOption'>
+              {""}
             </button>
           ))}
         </div>
-        <p className='score'>Score: {score}</p>
+        <p className='score' data-testid='score'>
+          Score: {score}
+        </p>
         <p className='power'>Power: {power} </p>
-        <button className='newGame' onClick={newGame}>
+        <button className='newGame' onClick={newGame} data-testid='newGameButton'>
           New Game
         </button>
+        {!options.length && <Info play={play} />}
       </div>
       {!power.length && <Modal newGame={newGame} message="You've Lost 🥲😢😭" />}
       {score > 9 && <Modal newGame={newGame} message='You Win🎉🍾😏' />}
@@ -91,17 +104,37 @@ const Modal: React.FC<ModalProps> = ({ newGame, message }) => {
     <div className='modal'>
       <div className='modal__content'>
         <h1>{message}</h1>
-        <button className='newGame' onClick={newGame}>
+        <button className='newGame' onClick={newGame} data-testid='newGameButton'>
           Play Again
         </button>
       </div>
     </div>
   )
 }
-// Define prop types for the Modal component
+
+const Info: React.FC<info> = ({ play }) => {
+  return (
+    <div className='modal' data-testid='gameInstructions'>
+      <div className='modal__content'>
+        <h1>How to Play!</h1>
+        <p>
+          Click on the co-responding color shown in the color options to gain points. <br /> Gain 10 points or 10 correct guesses to win, if your guess is wrong your power will be deducted and if
+          finished will result to game over.
+        </p>
+        <button className='newGame' onClick={play}>
+          Play
+        </button>
+      </div>
+    </div>
+  )
+}
+
 interface ModalProps {
   newGame: () => void
   message: string
+}
+interface info {
+  play: () => void
 }
 
 export default App
